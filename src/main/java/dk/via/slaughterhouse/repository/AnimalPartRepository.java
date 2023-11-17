0140238a52d657fdcb6dc44df0cfa059a941aa9e
Josan03 <josan.c2003@gmail.com>
2023-11-17 14:52:28 +0100

    Change AnimalPartRepository.

--- src/main/java/dk/via/slaughterhouse/repository/AnimalPartRepository.java
+++ src/main/java/dk/via/slaughterhouse/repository/AnimalPartRepository.java
@@ -4,8 +4,6 @@ import dk.via.slaughterhouse.model.AnimalPart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface AnimalPartRepository extends JpaRepository<AnimalPart, Long> {
 }
